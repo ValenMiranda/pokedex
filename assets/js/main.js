4d0e3470bfa36f6258fbd00b2c901cd5c3581ee5
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-07-07 14:01:47 -0400

    titulo, input, boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-
+	$.ajax({
+		url: 'http://pokeapi.co/api/v2/pokemon/',
+		type: 'GET',
+		dataType: 'JSON',
+		data: {"limit": '811'},
+	})
+	.done(function(respuesta) {
+		console.log(respuesta);
+		console.log("success");
+		namePoke(respuesta);
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	
 });
