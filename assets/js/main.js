cf82543c28d571dddc327d798a8f391ff7a81820
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-07-07 23:57:04 -0400

    imagenes pokemones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
 	$.ajax({
-		url: 'http://pokeapi.co/api/v2/pokemon/',
-		type: 'GET',
-		dataType: 'JSON',
-		data: {"limit": '811'},
-	})
-	.done(function(respuesta) {
-		console.log(respuesta);
-		console.log("success");
-		namePoke(respuesta);
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});
+        url: 'https://pokeapi.co/api/v2/pokemon/',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {"limit": '811'},
+    })
+    .done(function(result) {
+        console.log("success");
+        console.log(result);
+        /*recorriendo el contenido de la api*/
+        for (var i = 1; i < 719; i++) {
+            var poke = $("<img src=https://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
+            $('div.pokemon').append(poke);
+        }
+
+    })
+    .fail(function() {
+        console.log("error");
+    })
+    .always(function() {
+        console.log("complete");
+    })
 	
 });
